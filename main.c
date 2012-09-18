0684f601b7edd4dfdc2e7318e814d759e1d62f2d
Jeb Bradwell <jebradwell@gmail.com>
2012-09-17 20:10:00 -0700

    basic map loading initialization and printing
    Signed-off-by: Jeb Bradwell <jebradwell@gmail.com>

--- main.c
+++ main.c
@@ -5,44 +5,82 @@
 #define BOARD_WIDTH 80
 #define BOARD_HEIGHT 24
 
-int main(int argc, char *argv[])
+
+char **create_board(void) 
 {
-	
-	return 0;
-}
-
-char **create_board(void) {
 	int i, j;
-	for( j = 0; j < BOARD_HEIGHT; ++j) {
-		for( i = 0; i <  BOARD_WIDTH; ++i) {
-
+	char **new_board = (char**) malloc (BOARD_WIDTH * sizeof( char** ));
+	for( i = 0; i < BOARD_WIDTH; ++i) 
+	{
+		new_board[i] = (char*) malloc (BOARD_HEIGHT * sizeof( char* ));
+	}
+	//initialize board to blanks at first.
+	for( i = 0; i < BOARD_WIDTH; ++i) {
+		for( j = 0; j < BOARD_HEIGHT; ++j)
+		{
+			new_board[i][j] = ' ';
 		}
 	}
-	new_board = (char*) malloc (i+1);
+	return new_board;
 }
 
-void delete_board(char **board) {
-
+void delete_board(char **board) 
+{
+	int i;
+	for(i = 0; i < BOARD_WIDTH; ++i)
+	{
+		free(board[i]);
+	}
 }
 
-char** load_board(char *filename) {
+char** load_board(char *filename) 
+{
 	//Create variables at the start of the function as C mandates.
-	int file_close_return;
-	const char *filename = "board.txt";
+	int file_close_return, i, j;
+
 	char **new_board = create_board();
 	FILE *FPboard;
 
 	FPboard = fopen(filename, "r");
-
-	file_close_return = fclose(FPboard);
-	if(file_close_return) {
-		
-		printf("Could not close file properly. Filename : %s", filename);
-		return ;
+	if(FPboard != NULL)
+	{
+		char line [ BOARD_WIDTH ];
+		j = 0;
+		while ( fgets ( line, sizeof line, FPboard ) != NULL )
+		{
+			for( i = 0; i < BOARD_WIDTH; ++i )
+			{
+				if(line[i] != NULL)
+				{
+					new_board[i][j] = line[i];
+				}
+				else
+				{
+					new_board[i][j] = " ";
+				}
+			}
+			++j; // make sure to increment the height
+		}
+		file_close_return = fclose(FPboard);
+		if(file_close_return) 
+		{
+			printf("Could not close file : %s", filename);
+			perror ( filename );
+		}
 	}
+	else 
+	{
+		printf("Could not open file : %s", filename);
+		perror ( filename );
+	}
+	return new_board;
 }
 
-void print_board(char **board)
+void add_line(char line[BOARD_WIDTH], char **board) 
+{
+	
+}
+void print_board(char **board) 
 {
 	int i, j;
 	for( j = 0; j < BOARD_HEIGHT; ++j)
@@ -53,3 +91,11 @@ void print_board(char **board)
 		}
 	}
 }
+
+int main(int argc, char *argv[]) {
+	char *filename = "board.txt";
+	char **board = load_board(filename);
+	print_board(board);
+	return 0;
+}
+
